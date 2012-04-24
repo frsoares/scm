98dee8bfde0aa0f259ffe0c34e43b93bd487f6da
Fernando Castor <castor@cin.ufpe.br>
2012-04-24 14:02:04 -0300

    Bratislava.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 
     public static void main(String args[]) {
-        System.out.println("Hello world!");
+        System.out.println("Bratislava!");
+        System.err.println("Hello world!");
     } 
 
 }
